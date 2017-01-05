9aeca103b68420753ff9cc1f591063ca22a75384
Tim Foerster <github@mailserver.1n3t.de>
2017-01-05 21:20:36 +0100

    Init new gem
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in sain_smart-network_IO.gemspec
+gemspec
---
+++ lib/sain_smart/network_IO.rb
@@ -0,0 +1,7 @@
+require "sain_smart/network_IO/version"
+
+module SainSmart
+  module NetworkIO
+    # Your code goes here...
+  end
+end
